8b4102c6ebef05a7a04f59479474adba844b3b1c
geofrocker <geofrocker2@gmail.com>
2018-01-09 14:23:02 +0300

    Refactor/Use axios to make api calls

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,12 @@
-const url = 'http://127.0.0.1:5000/';
-export default url;
+import axios from 'axios';
+
+export const url = 'http://127.0.0.1:5000/';
+
+export const http = axios.create({
+    // baseURL: url,
+    headers: {
+        'x-access-token': sessionStorage.getItem('token')
+    },
+  });
+
+  export default url;
