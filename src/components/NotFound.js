411d2f1eab08ac2cc96e95677e21f23b0a8c116e
geofrocker <geofrocker2@gmail.com>
2018-01-09 14:20:31 +0300

    Refactor/Add class name to correspond with css

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,9 +2,10 @@
 import React from 'react';
 
 const NotFound = () =>
-  <div>
+  <div class="NotFound">
     <h3>404 page not found</h3>
     <p>We are sorry but the page you are looking for does not exist.</p>
+    <a className="btn btn-primary" href="/">Take me Home</a>
   </div>
 
 export default NotFound;
