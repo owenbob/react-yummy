9431aec10a7023de9f48588c16d49e3956068e4a
geofrocker <geofrocker2@gmail.com>
2018-01-14 23:55:21 +0300

    CHORE/update url link from local to heroku

--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const url = 'http://127.0.0.1:5000/';
+export const url = 'https://yummy-api.herokuapp.com/';
 
 export const http = axios.create({
     // baseURL: url,
